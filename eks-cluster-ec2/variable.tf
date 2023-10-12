9266c8b8486068991283df3d2e5a88ac9e65209f
ferdinand <momeniferdinand@yahoo.com>
2023-10-11 20:01:38 -0500

    chnaged in the whole project

--- eks-cluster-ec2/variable.tf
+++ eks-cluster-ec2/variable.tf
@@ -3,7 +3,7 @@ variable "cluster_name" {
 }
 
 variable "cluster_version" {
-  default = "1.25"
+  default = "1.27"
 }
 
 variable "region" {
@@ -14,3 +14,4 @@ variable "ingress_ports" {
   description = "Managed node groups use this security group for control-plane-to-data-plane communication."
   default     = ["443", "8080", "80", "9090", "9443", "2049"]
 }
+
